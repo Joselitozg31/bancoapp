517350a7e2f901a572766a908d8e64007e33e826
lromnav497 <lromnav497@g.educaand.es>
2025-02-25 16:59:12 +0100

    instalacion de mysql y variables de entorno .env

---
+++ src/lib/database.js
@@ -0,0 +1,15 @@
+// lib/db.js
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+export default pool;
