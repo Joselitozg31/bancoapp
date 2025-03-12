0cfb885623c0888a456e331ae298d682ec3dded6
Josezarz <146075151+Joselitozg31@users.noreply.github.com>
2025-03-12 14:57:17 +0100

    dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -5,20 +5,22 @@ import Header from '@/components/header';
 
 const DashboardPage = () => {
   const [transactions, setTransactions] = useState([]);
-  const [userDocumentNumber, setUserDocumentNumber] = useState('');
+  const [userDocumentId, setUserDocumentId] = useState('');
   const [accountCurrencies, setAccountCurrencies] = useState({});
+  const [accounts, setAccounts] = useState([]);
+  const [cards, setCards] = useState([]);
 
   useEffect(() => {
     // Obtener el objeto user desde localStorage
     const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.document_number) {
-      const userDocumentNumber = user.document_number;
-      setUserDocumentNumber(userDocumentNumber);
+    if (user && user.document_id) {
+      const userDocumentId = user.document_id;
+      setUserDocumentId(userDocumentId);
 
       const fetchTransactions = async () => {
         try {
-          const response = await axios.get('/api/transactions', {
-            params: { user_document_number: userDocumentNumber }
+          const response = await axios.get('/api/dashboard/transactions', {
+            params: { user_document_id: userDocumentId }
           });
           setTransactions(response.data);
         } catch (error) {
@@ -26,7 +28,31 @@ const DashboardPage = () => {
         }
       };
 
+      const fetchAccounts = async () => {
+        try {
+          const response = await axios.get('/api/dashboard/accounts/list_account', {
+            params: { user_document_id: userDocumentId }
+          });
+          setAccounts(response.data);
+        } catch (error) {
+          console.error('Error fetching accounts:', error);
+        }
+      };
+
+      const fetchCards = async () => {
+        try {
+          const response = await axios.get('/api/dashboard/cards/list_card', {
+            params: { user_document_id: userDocumentId }
+          });
+          setCards(response.data);
+        } catch (error) {
+          console.error('Error fetching cards:', error);
+        }
+      };
+
       fetchTransactions();
+      fetchAccounts();
+      fetchCards();
     }
   }, []);
 
@@ -37,7 +63,7 @@ const DashboardPage = () => {
 
       for (const iban of uniqueIbans) {
         try {
-          const response = await axios.get('/api/dashboard/accounts', {
+          const response = await axios.get('/api/dashboard/accounts/list_account', {
             params: { iban }
           });
           currencies[iban] = response.data.currency;
@@ -85,6 +111,25 @@ const DashboardPage = () => {
       ));
   };
 
+  const renderAccounts = () => {
+    return accounts.map(account => (
+      <tr key={account.iban} className="bg-transparent">
+        <td className="bg-transparent text-white rounded-lg">{account.iban}</td>
+        <td className="bg-transparent text-white rounded-lg">{account.balance} {getCurrencySymbol(account.currency)}</td>
+      </tr>
+    ));
+  };
+
+  const renderCards = () => {
+    return cards.map(card => (
+      <tr key={card.card_number} className="bg-transparent">
+        <td className="bg-transparent text-white rounded-lg">{card.card_number}</td>
+        <td className="bg-transparent text-white rounded-lg">{card.card_type}</td>
+        <td className="bg-transparent text-white rounded-lg">{card.expiry_date}</td>
+      </tr>
+    ));
+  };
+
   const getCurrencySymbol = (currency) => {
     switch (currency) {
       case 'USD':
@@ -104,6 +149,32 @@ const DashboardPage = () => {
       <div className="container max-w-3xl p-8 mt-16">
         <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
         <div className="flex flex-wrap justify-between space-y-12">
+          <div className="w-full md:w-1/3 mb-24 mt-12">
+            <h2 className="text-2xl font-bold mb-4">Listar Cuenta</h2>
+            <div className="overflow-y-auto max-h-64 bg-gradient-to-br from-blue-900 to-slate-900 rounded-lg" style={{ scrollbarColor: 'gray transparent', scrollbarWidth: 'thin' }}>
+              <table className="w-full bg-transparent rounded-lg shadow-md">
+                <thead>
+                  <tr>
+                    <th className="text-white">IBAN</th><th className="text-white">Balance</th>
+                  </tr>
+                </thead>
+                <tbody>{renderAccounts()}</tbody>
+              </table>
+            </div>
+          </div>
+          <div className="w-full md:w-1/3 mb-24 mt-12">
+            <h2 className="text-2xl font-bold mb-4">Tarjetas</h2>
+            <div className="overflow-y-auto max-h-64 bg-gradient-to-br from-blue-900 to-slate-900 rounded-lg" style={{ scrollbarColor: 'gray transparent', scrollbarWidth: 'thin' }}>
+              <table className="w-full bg-transparent rounded-lg shadow-md">
+                <thead>
+                  <tr>
+                    <th className="text-white">Número de Tarjeta</th><th className="text-white">Tipo</th><th className="text-white">Fecha de Expiración</th>
+                  </tr>
+                </thead>
+                <tbody>{renderCards()}</tbody>
+              </table>
+            </div>
+          </div>
           <div className="w-full md:w-1/3 mb-24 mt-12">
             <h2 className="text-2xl font-bold mb-4">Ingresos Recientes</h2>
             <div className="overflow-y-auto max-h-64 bg-gradient-to-br from-blue-900 to-slate-900 rounded-lg" style={{ scrollbarColor: 'gray transparent', scrollbarWidth: 'thin' }}>
