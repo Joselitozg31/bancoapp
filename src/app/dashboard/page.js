6c83457cb5280d8e0d97663349ff62bf82208c78
Josezarz <146075151+Joselitozg31@users.noreply.github.com>
2025-03-12 16:20:50 +0100

    dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -2,59 +2,78 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from '@/components/header';
+import { useRouter } from 'next/navigation';
 
 const DashboardPage = () => {
   const [transactions, setTransactions] = useState([]);
-  const [userDocumentId, setUserDocumentId] = useState('');
+  const [userDocumentNumber, setUserDocumentNumber] = useState('');
   const [accountCurrencies, setAccountCurrencies] = useState({});
   const [accounts, setAccounts] = useState([]);
   const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
     // Obtener el objeto user desde localStorage
     const user = JSON.parse(localStorage.getItem('user'));
-    if (user && user.document_id) {
-      const userDocumentId = user.document_id;
-      setUserDocumentId(userDocumentId);
+    if (user && user.document_number) {
+      const userDocumentNumber = user.document_number;
+      setUserDocumentNumber(userDocumentNumber);
 
       const fetchTransactions = async () => {
         try {
           const response = await axios.get('/api/dashboard/transactions', {
-            params: { user_document_id: userDocumentId }
+            headers: {
+              'Content-Type': 'application/json',
+              'document_number': userDocumentNumber
+            }
           });
           setTransactions(response.data);
         } catch (error) {
           console.error('Error fetching transactions:', error);
+          setError(error.message);
         }
       };
 
       const fetchAccounts = async () => {
         try {
           const response = await axios.get('/api/dashboard/accounts/list_account', {
-            params: { user_document_id: userDocumentId }
+            headers: {
+              'Content-Type': 'application/json',
+              'document_number': userDocumentNumber
+            }
           });
           setAccounts(response.data);
         } catch (error) {
           console.error('Error fetching accounts:', error);
+          setError(error.message);
         }
       };
 
       const fetchCards = async () => {
         try {
           const response = await axios.get('/api/dashboard/cards/list_card', {
-            params: { user_document_id: userDocumentId }
+            headers: {
+              'Content-Type': 'application/json',
+              'document_number': userDocumentNumber
+            }
           });
           setCards(response.data);
         } catch (error) {
           console.error('Error fetching cards:', error);
+          setError(error.message);
         }
       };
 
       fetchTransactions();
       fetchAccounts();
       fetchCards();
+      setLoading(false);
+    } else {
+      router.push('/login');
     }
-  }, []);
+  }, [router]);
 
   useEffect(() => {
     const fetchAccountCurrencies = async () => {
@@ -114,8 +133,24 @@ const DashboardPage = () => {
   const renderAccounts = () => {
     return accounts.map(account => (
       <tr key={account.iban} className="bg-transparent">
-        <td className="bg-transparent text-white rounded-lg">{account.iban}</td>
-        <td className="bg-transparent text-white rounded-lg">{account.balance} {getCurrencySymbol(account.currency)}</td>
+        <td className="bg-transparent text-white rounded-lg">
+          <a
+            onClick={() => router.push(`/dashboard/accounts/details_account?iban=${account.iban}`)}
+            className="text-blue-500 hover:underline cursor-pointer"
+          >
+            {account.iban}
+          </a>
+        </td>
+        <td className="bg-transparent text-white rounded-lg">{account.total_balance} {getCurrencySymbol(account.currency)}</td>
+        <td className="bg-transparent text-white rounded-lg">{new Date(account.opening_date).toLocaleDateString()}</td>
+        <td className="bg-transparent text-white rounded-lg">
+          <button
+            onClick={() => router.push(`/dashboard/accounts/close_account?iban=${account.iban}`)}
+            className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+          >
+            Dar de baja
+          </button>
+        </td>
       </tr>
     ));
   };
@@ -125,7 +160,18 @@ const DashboardPage = () => {
       <tr key={card.card_number} className="bg-transparent">
         <td className="bg-transparent text-white rounded-lg">{card.card_number}</td>
         <td className="bg-transparent text-white rounded-lg">{card.card_type}</td>
-        <td className="bg-transparent text-white rounded-lg">{card.expiry_date}</td>
+        <td className="bg-transparent text-white rounded-lg">{new Date(card.expiration_date).toLocaleDateString()}</td>
+        <td className="bg-transparent text-white rounded-lg">{new Date(card.hiring_date).toLocaleDateString()}</td>
+        <td className="bg-transparent text-white rounded-lg">{card.ccv}</td>
+        <td className="bg-transparent text-white rounded-lg">{card.account_iban}</td>
+        <td className="bg-transparent text-white rounded-lg">
+          <button
+            onClick={() => router.push(`/dashboard/cards/close_card?cardNumber=${card.card_number}`)}
+            className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600"
+          >
+            Dar de baja
+          </button>
+        </td>
       </tr>
     ));
   };
@@ -143,71 +189,89 @@ const DashboardPage = () => {
     }
   };
 
+  if (loading) return <div>Cargando...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div className="min-h-full flex flex-col items-center justify-center bg-gradient-to-br from-blue-900 to-slate-900 text-white py-6">
       <Header />
       <div className="container max-w-3xl p-8 mt-16">
         <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
         <div className="flex flex-wrap justify-between space-y-12">
-          <div className="w-full md:w-1/3 mb-24 mt-12">
+          <div className="w-full mb-24 mt-12">
             <h2 className="text-2xl font-bold mb-4">Listar Cuenta</h2>
             <div className="overflow-y-auto max-h-64 bg-gradient-to-br from-blue-900 to-slate-900 rounded-lg" style={{ scrollbarColor: 'gray transparent', scrollbarWidth: 'thin' }}>
               <table className="w-full bg-transparent rounded-lg shadow-md">
                 <thead>
                   <tr>
-                    <th className="text-white">IBAN</th><th className="text-white">Balance</th>
+                    <th className="text-white">IBAN</th>
+                    <th className="text-white">Saldo</th>
+                    <th className="text-white">Fecha de Apertura</th>
+                    <th className="text-white">Acciones</th>
                   </tr>
                 </thead>
                 <tbody>{renderAccounts()}</tbody>
               </table>
             </div>
           </div>
-          <div className="w-full md:w-1/3 mb-24 mt-12">
+          <div className="w-full mb-24 mt-12">
             <h2 className="text-2xl font-bold mb-4">Tarjetas</h2>
             <div className="overflow-y-auto max-h-64 bg-gradient-to-br from-blue-900 to-slate-900 rounded-lg" style={{ scrollbarColor: 'gray transparent', scrollbarWidth: 'thin' }}>
               <table className="w-full bg-transparent rounded-lg shadow-md">
                 <thead>
                   <tr>
-                    <th className="text-white">Número de Tarjeta</th><th className="text-white">Tipo</th><th className="text-white">Fecha de Expiración</th>
+                    <th className="text-white">Número de Tarjeta</th>
+                    <th className="text-white">Tipo de Tarjeta</th>
+                    <th className="text-white">Fecha de Expiración</th>
+                    <th className="text-white">Fecha de Contratación</th>
+                    <th className="text-white">CCV</th>
+                    <th className="text-white">IBAN de la Cuenta</th>
+                    <th className="text-white">Acciones</th>
                   </tr>
                 </thead>
                 <tbody>{renderCards()}</tbody>
               </table>
             </div>
           </div>
-          <div className="w-full md:w-1/3 mb-24 mt-12">
+          <div className="w-full mb-24 mt-12">
             <h2 className="text-2xl font-bold mb-4">Ingresos Recientes</h2>
             <div className="overflow-y-auto max-h-64 bg-gradient-to-br from-blue-900 to-slate-900 rounded-lg" style={{ scrollbarColor: 'gray transparent', scrollbarWidth: 'thin' }}>
               <table className="w-full bg-transparent rounded-lg shadow-md">
                 <thead>
                   <tr>
-                    <th className="text-white">Monto</th><th className="text-white">Fecha</th><th className="text-white">Concepto</th>
+                    <th className="text-white">Monto</th>
+                    <th className="text-white">Fecha</th>
+                    <th className="text-white">Concepto</th>
                   </tr>
                 </thead>
                 <tbody>{renderTransactions('income')}</tbody>
               </table>
             </div>
           </div>
-          <div className="w-full md:w-1/3 mb-24 mt-12">
+          <div className="w-full mb-24 mt-12">
             <h2 className="text-2xl font-bold mb-4">Gastos Recientes</h2>
             <div className="overflow-y-auto max-h-64 bg-gradient-to-br from-blue-900 to-slate-900 rounded-lg" style={{ scrollbarColor: 'gray transparent', scrollbarWidth: 'thin' }}>
               <table className="w-full bg-transparent rounded-lg shadow-md">
                 <thead>
                   <tr>
-                    <th className="text-white">Monto</th><th className="text-white">Fecha</th><th className="text-white">Concepto</th>
+                    <th className="text-white">Monto</th>
+                    <th className="text-white">Fecha</th>
+                    <th className="text-white">Concepto</th>
                   </tr>
                 </thead>
                 <tbody>{renderTransactions('expense')}</tbody>
               </table>
             </div>
           </div>
-          <div className="w-full md:w-1/3 mb-24 mt-12">
+          <div className="w-full mb-24 mt-12">
             <h2 className="text-2xl font-bold mb-4">Retenidos</h2>
             <div className="overflow-y-auto max-h-64 bg-gradient-to-br from-blue-900 to-slate-900 rounded-lg" style={{ scrollbarColor: 'gray transparent', scrollbarWidth: 'thin' }}>
               <table className="w-full bg-transparent rounded-lg shadow-md">
                 <thead>
                   <tr>
-                    <th className="text-white">Monto</th><th className="text-white">Fecha</th><th className="text-white">Concepto</th>
+                    <th className="text-white">Monto</th>
+                    <th className="text-white">Fecha</th>
+                    <th className="text-white">Concepto</th>
                   </tr>
                 </thead>
                 <tbody>{renderAllHeldTransactions()}</tbody>
